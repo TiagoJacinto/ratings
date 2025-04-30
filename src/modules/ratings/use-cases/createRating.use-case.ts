5d3d293a45741eb7372ec7684f1258209b6c9169
Tiago <tiagojacintodev@gmail.com>
2025-04-30 12:20:28 +0100

    feat: implement rating creation use case

---
+++ src/modules/ratings/use-cases/createRating.use-case.ts
@@ -0,0 +1,48 @@
+import { Weight } from '@/modules/weights/domain/Weight';
+import { Weights } from '@/modules/weights/domain/Weights';
+import { WeightValue } from '@/modules/weights/domain/WeightValue';
+import { ok, Result } from '@/shared/core/Result';
+import { type UseCase } from '@/shared/core/UseCase';
+
+import { Rating } from '../domain/Rating';
+import { type RatingRepository } from '../repositories/rating/rating.repository';
+
+export type CreateRatingDTO = {
+  name: string;
+  description?: string;
+  weights: {
+    name: string;
+    value: number;
+  }[];
+};
+
+type Response = Result<number, Error>;
+
+export class CreateRatingUseCase implements UseCase<CreateRatingDTO, Response> {
+  constructor(private readonly ratingRepository: RatingRepository) {}
+
+  async execute(request: CreateRatingDTO): Promise<Response> {
+    const weightsOrError = Result.combine(
+      request.weights.map((w) => () => {
+        const value = WeightValue.create({ value: w.value });
+        if (!value.isOk) return value;
+
+        return ok(
+          Weight.create({
+            name: w.name,
+            value: value.value,
+          }),
+        );
+      }),
+    );
+    if (!weightsOrError.isOk) return weightsOrError;
+
+    const rating = Rating.create({
+      name: request.name,
+      description: request.description,
+      weights: Weights.create(weightsOrError.value),
+    });
+
+    return ok(await this.ratingRepository.save(rating));
+  }
+}
