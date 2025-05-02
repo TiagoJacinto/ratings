ec1b778d41dc3771e26da512d0cffe8b035e10d2
Tiago <tiagojacintodev@gmail.com>
2025-05-02 12:34:36 +0100

    refactor: remove private constructor definition

--- src/modules/weights/domain/Weights.ts
+++ src/modules/weights/domain/Weights.ts
@@ -3,15 +3,11 @@ import { WatchedList } from '@/shared/domain/WatchedList';
 import { type Weight } from './Weight';
 
 export class Weights extends WatchedList<Weight> {
-  private constructor(initialWeights?: Weight[]) {
-    super(initialWeights);
+  static create(weights?: Weight[]) {
+    return new this(weights);
   }
 
   compareItems(a: Weight, b: Weight): boolean {
     return a.equals(b);
-  }
-
-  static create(weights?: Weight[]) {
-    return new this(weights);
   }
 }
