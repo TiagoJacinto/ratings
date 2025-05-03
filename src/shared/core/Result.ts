5e7396961e0bee34e80615d4c02e7dc74f235529
Tiago <tiagojacintodev@gmail.com>
2025-05-03 12:49:23 +0100

    feat: improve Result abstraction

--- src/shared/core/Result.ts
+++ src/shared/core/Result.ts
@@ -1,47 +1,49 @@
 import { toPairs } from 'ramda';
 import { type UnknownRecord, type Simplify, type ValueOf } from 'type-fest';
 
-export type Ok<T> = {
-  value: T;
-  isOk: true;
-  get unwrapped(): T;
-};
+export class Ok<T> {
+  readonly isOk = true;
+  readonly value: T;
 
-export type Err<E> = Readonly<{
-  error: E;
-  isOk: false;
-  get unwrapped(): never;
-}>;
+  constructor(value: T) {
+    this.value = value;
+  }
+
+  get unwrapped() {
+    return this.value;
+  }
+}
+
+export class Err<E> {
+  readonly isOk = false;
+  readonly error: E;
+
+  constructor(error: E) {
+    this.error = error;
+  }
+
+  get unwrapped() {
+    throw new Error("This value is an error and can't be unwrapped.");
+  }
+}
 
 export type Result<T, E> = Ok<T> | Err<E>;
 
 export function ok(): Ok<undefined>;
 export function ok<T>(value: T): Ok<T>;
 export function ok(value?: unknown) {
-  return {
-    isOk: true,
-    value,
-    get unwrapped() {
-      return value;
-    },
-  };
+  return new Ok(value);
 }
 
 export function err(): Err<undefined>;
 export function err<E>(error: E): Err<E>;
 export function err(error?: unknown) {
-  return {
-    isOk: false,
-    error,
-    get unwrapped() {
-      throw error;
-    },
-  };
+  return new Err(error);
 }
 
 export namespace Result {
   type MappedReturnType<T> = {
-    [i in keyof T]: T[i] extends () => infer TOk ? TOk : never;
+    [i in keyof T]: T[i] extends () => infer TOk ? TOk : T[i];
   };
 
   type MappedOkValues<T> = Simplify<{
@@ -52,44 +54,28 @@ export namespace Result {
     ValueOf<T, TValue> extends Result<unknown, infer TErr> ? TErr : never;
 
   type UnknownResult = Result<unknown, unknown>;
-  type TupleOfUnknownResults = readonly UnknownResult[];
-  type TupleOfUnknownResultsGetter = readonly (() => UnknownResult)[];
-  type RecordOfUnknownResults = Record<PropertyKey, UnknownResult>;
-  type RecordOfUnknownResultsGetter = Record<PropertyKey, () => UnknownResult>;
+  type TupleOfUnknownResults = readonly (UnknownResult | (() => UnknownResult))[];
+  type RecordOfUnknownResults = Record<PropertyKey, UnknownResult | (() => UnknownResult)>;
 
   export function combine<const TResultsTuple extends TupleOfUnknownResults>(
     results: TResultsTuple,
-  ): Result<MappedOkValues<TResultsTuple>, GetErrValue<TResultsTuple, number>>;
-
-  export function combine<const TResultsGetterTuple extends TupleOfUnknownResultsGetter>(
-    results: TResultsGetterTuple,
   ): Result<
-    MappedOkValues<MappedReturnType<TResultsGetterTuple>>,
-    GetErrValue<MappedReturnType<TResultsGetterTuple>, number>
+    MappedOkValues<MappedReturnType<TResultsTuple>>,
+    GetErrValue<MappedReturnType<TResultsTuple>, number>
   >;
 
   export function combine<TResultsRecord extends RecordOfUnknownResults>(
     results: TResultsRecord,
-  ): Result<MappedOkValues<TResultsRecord>, GetErrValue<TResultsRecord>>;
-
-  export function combine<TResultsGetterRecord extends RecordOfUnknownResultsGetter>(
-    results: TResultsGetterRecord,
   ): Result<
-    MappedOkValues<MappedReturnType<TResultsGetterRecord>>,
-    GetErrValue<MappedReturnType<TResultsGetterRecord>>
+    MappedOkValues<MappedReturnType<TResultsRecord>>,
+    GetErrValue<MappedReturnType<TResultsRecord>>
   >;
 
-  export function combine(
-    results:
-      | TupleOfUnknownResults
-      | TupleOfUnknownResultsGetter
-      | RecordOfUnknownResults
-      | RecordOfUnknownResultsGetter,
-  ) {
+  export function combine(results: TupleOfUnknownResults | RecordOfUnknownResults) {
     if (Array.isArray(results)) {
       const unwrappedOkValues = [];
 
-      for (const resultOrGetter of results as TupleOfUnknownResults | TupleOfUnknownResultsGetter) {
+      for (const resultOrGetter of results as TupleOfUnknownResults) {
         const result = typeof resultOrGetter === 'function' ? resultOrGetter() : resultOrGetter;
 
         if (!result.isOk) return result;
@@ -102,9 +88,7 @@ export namespace Result {
 
     const unwrappedOkValues: UnknownRecord = {};
 
-    for (const [key, resultOrGetter] of toPairs(
-      results as RecordOfUnknownResults | RecordOfUnknownResultsGetter,
-    )) {
+    for (const [key, resultOrGetter] of toPairs(results as RecordOfUnknownResults)) {
       const result = typeof resultOrGetter === 'function' ? resultOrGetter() : resultOrGetter;
 
       if (!result.isOk) return result;
