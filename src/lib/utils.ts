11cd4e83f382ec4bc572d3a632ac34be659d47ed
Tiago <tiagojacintodev@gmail.com>
2025-05-08 15:43:33 +0100

    feat: implement step by function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const stepBy = (fractionDigits: number) => +`0.${'0'.repeat(fractionDigits - 1)}1`;
