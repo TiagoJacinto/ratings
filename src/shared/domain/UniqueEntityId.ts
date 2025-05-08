13b555a0a0a9d5b72adee86b3b9a2aff969a3e1b
Tiago <tiagojacintodev@gmail.com>
2025-05-08 17:04:59 +0100

    feat: define raw id type

--- src/shared/domain/UniqueEntityId.ts
+++ src/shared/domain/UniqueEntityId.ts
@@ -1,9 +1,10 @@
 import { randomUUID } from 'crypto';
 
 import { Identifier } from './Identifier';
+import { type RawId } from './RawId';
 
-export class UniqueEntityID extends Identifier<string | number> {
-  constructor(id?: string | number) {
+export class UniqueEntityID extends Identifier<RawId> {
+  constructor(id?: RawId) {
     super(id ?? randomUUID?.() ?? crypto.randomUUID());
   }
 }
