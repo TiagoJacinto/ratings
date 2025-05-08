cc6fed5cb6cf6942d6e0d8686272a113b931a61b
Tiago <tiagojacintodev@gmail.com>
2025-05-08 15:44:22 +0100

    feat: validate weight value step

--- src/view/domain/WeightValue.ts
+++ src/view/domain/WeightValue.ts
@@ -1,3 +1,4 @@
+import { stepBy } from '@/lib/utils';
 import { z } from 'zod';
 
 export namespace WeightValue {
@@ -6,5 +7,9 @@ export namespace WeightValue {
   export const MAX_FRACTION_DIGITS = 1;
 }
 
-export const WeightValueSchema = z.number().min(WeightValue.MIN).max(WeightValue.MAX);
+export const WeightValueSchema = z
+  .number()
+  .min(WeightValue.MIN)
+  .max(WeightValue.MAX)
+  .step(stepBy(WeightValue.MAX_FRACTION_DIGITS));
 export type WeightValue = z.infer<typeof WeightValueSchema>;
