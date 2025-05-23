8d64561b8b0ad9874bf31e3e38c1d00ba9edc236
Tiago <tiagojacintodev@gmail.com>
2025-05-23 19:10:01 +0100

    revert: remove weight value step validation

--- src/view/domain/WeightValue.ts
+++ src/view/domain/WeightValue.ts
@@ -1,4 +1,3 @@
-import { stepBy } from '@/lib/utils';
 import { z } from 'zod';
 
 export namespace WeightValue {
@@ -7,9 +6,5 @@ export namespace WeightValue {
   export const MAX_FRACTION_DIGITS = 1;
 }
 
-export const WeightValueSchema = z
-  .number()
-  .min(WeightValue.MIN)
-  .max(WeightValue.MAX)
-  .step(stepBy(WeightValue.MAX_FRACTION_DIGITS));
+export const WeightValueSchema = z.number().min(WeightValue.MIN).max(WeightValue.MAX);
 export type WeightValue = z.infer<typeof WeightValueSchema>;
