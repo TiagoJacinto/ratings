741df014c0fdf5029a9a3784663ea71041c5ea6b
Tiago <tiagojacintodev@gmail.com>
2025-05-29 19:26:34 +0100

    fix: file picker button style

--- src/components/molecules/OpenFilePickerButton.tsx
+++ src/components/molecules/OpenFilePickerButton.tsx
@@ -1,21 +1,22 @@
-import { cn } from '@/lib/utils';
 import { showOpenFilePicker } from 'file-system-access';
 import { type CustomOpenFilePickerOptions } from 'file-system-access/lib/showOpenFilePicker';
 import * as React from 'react';
 
-type OpenFilePickerButtonProps = React.ComponentProps<"button"> & {
+import { cn } from '@/lib/utils';
+
+type OpenFilePickerButtonProps = React.ComponentProps<'button'> & {
   handle?: FileSystemFileHandle;
-  onFileChange?: (value: FileSystemFileHandle) => void;
   options?: CustomOpenFilePickerOptions;
+  onFileChange?: (value: FileSystemFileHandle) => void;
 };
 
 const OpenFilePickerButton = React.forwardRef<HTMLButtonElement, OpenFilePickerButtonProps>(
-  ({ className, options, onFileChange, ...props }, ref) => {
+  ({ className, onFileChange, options, ...props }, ref) => {
     return (
       <button
         className={cn(
-          'flex h-10 space-x-2 cursor-pointer w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-          className
+          'border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full cursor-pointer items-center space-x-2 rounded-md border bg-transparent px-3 py-2 text-base shadow-sm transition-colors focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          className,
         )}
         onClick={
           onFileChange &&
@@ -28,13 +29,13 @@ const OpenFilePickerButton = React.forwardRef<HTMLButtonElement, OpenFilePickerB
         ref={ref}
         {...props}
       >
-        <span className='border-0 bg-transparent text-sm font-medium text-foreground'>
+        <span className='text-foreground border-0 bg-transparent text-sm font-medium'>
           Choose File
         </span>
         <span>{props.handle?.name ?? 'No file chosen'}</span>
       </button>
     );
-  }
+  },
 );
 OpenFilePickerButton.displayName = 'OpenFilePickerButton';
 
