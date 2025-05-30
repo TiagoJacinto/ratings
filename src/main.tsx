cc0c620873fcec360db209b80da3d883b11bb09e
Tiago <tiagojacintodev@gmail.com>
2025-05-30 12:53:07 +0100

    feat: add full offline pwa support

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,23 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 
 import './index.css';
 
+import { registerSW } from 'virtual:pwa-register';
+
 import { Toaster } from '@/components/atoms/sonner';
 
 import { AppLayout } from './components/layouts/AppLayout';
 import { CreateAlternativeCategoryPage } from './pages/categories/new/page';
 import { AlternativeCategoryPage } from './pages/categories/{id}/page';
 import { CategoriesPage } from './pages/categories/page';
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('New content available. Reload?')) {
+      void updateSW(true);
+    }
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
